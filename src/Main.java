b7cef66f8af94289a6b344b47ff748383111da29
Nikolai Kiselev <kisnikn@mail.ru>
2020-10-29 21:17:18 +0300

    Main class and report added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int total = 2_000_000_000;
+        int transfer = 500_000_000;
+        total = total + transfer;
+        System.out.println("Сумма на счете составляет " +total+ " у.е.");
+    }
+}
